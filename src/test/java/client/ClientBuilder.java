dbd6e192eeff9d79c33f9d7f2954936488df7343
ViniciusKrau <viniciuskrau@gmail.com>
2024-05-04 22:14:10 -0300

    Finalizado

--- src/test/java/client/ClientBuilder.java
+++ src/test/java/client/ClientBuilder.java
@@ -3,18 +3,13 @@ package client;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClientBuilder {
     public static void main(String[] args) throws InterruptedException {
-        List<Process> processes = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-
+        for (int i = 0; i < 100; i++) {
             try {
                 ProcessBuilder processBuilder = new ProcessBuilder("java", "-cp", System.getProperty("java.class.path"), "client.Client");
                 Process process = processBuilder.start();
-                processes.add(process);
 
                 OutputStream stdin = process.getOutputStream();
                 PrintWriter writer = new PrintWriter(stdin);
@@ -26,12 +21,7 @@ public class ClientBuilder {
                 e.printStackTrace();
             }
         }
+
         System.out.println("All processes started.");
-
-        for (Process process : processes) {
-            process.destroy();
-        }
-        System.out.println("All processes closed.");
-
     }
 }
